562d407e61787f69826c5d0ca87538f0b96ebf03
Truerror <uitiong@live.com>
2017-04-22 20:23:55 +0800

    Made some routes in main.py

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
 from flask import Flask 
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return "Hello there!"
+
+
+@app.route("/admin")
+def admin():
+    return "Admin page here."
+
+
+@app.route("/admin/newpost")
+def new_post():
+    return "New post."
+
+
+@app.route("/archive")
+def archive():
+    return "Archive."
